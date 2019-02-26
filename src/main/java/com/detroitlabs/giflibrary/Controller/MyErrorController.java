31a9d78d0c63140449e3d3b76a71c3b021dfdc78
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-02-26 14:30:28 -0500

    Deletes out unused imports in Error Controller to clean up the class

--- src/main/java/com/detroitlabs/giflibrary/Controller/MyErrorController.java
+++ src/main/java/com/detroitlabs/giflibrary/Controller/MyErrorController.java
@@ -3,14 +3,10 @@ package com.detroitlabs.giflibrary.Controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
-import javax.xml.ws.Response;
 
 @Controller
 public class MyErrorController implements ErrorController {
